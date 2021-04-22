45d4e68921d857933e7fe67e89d532fcbd684de3
bernard <bvdv9075@gmail.com>
2021-04-22 12:05:22 +0100

    Added inclusion list

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ logging.basicConfig(
     filename="logs_" + today_date + ".log", level="INFO", format=log_format
 )
 
-inclusion_list = ["GAPS-97", "GAPS-96", "GAPS-95"]
+inclusion_list = ["GAPS-17", "GAPS-20", "GAPS-89", "GAPS-88", "GAPS-93", "GAPS-25", "GAPS-30","GAPS-87","GAPS-24",
+                  "GAPS-26", "GAPS-29", "GAPS-23", "GAPS-27", "GAPS-21", "GAPS-22", "GAPS-28", "GAPS-90", "GAPS-91",
+                  "GAPS-92", "GAPS-36", "GAPS-37", "GAPS-38", "GAPS-39", "GAPS-40", "GAPS-48", "GAPS-49", "GAPS-50",
+                  "GAPS-97", "GAPS-51", "GAPS-52", "GAPS-53", "GAPS-54", "GAPS-9", "GAPS-47", "GAPS-56", "GAPS-57",
+                  "GAPS-59", "GAPS-60", "GAPS-61", "GAPS-62", "GAPS-63", "GAPS-64", "GAPS-66", "GAPS-67", "GAPS-68",
+                  "GAPS-69", "GAPS-70", "GAPS-71", "GAPS-72", "GAPS-73", "GAPS-74", "GAPS-79", "GAPS-80", "GAPS-81",
+                  "GAPS-82", "GAPS-31", "GAPS-33", "GAPS-34", "GAPS-45", "GAPS-46"]
 
 
 def create_table_of_contents(csv_data):
@@ -30,7 +36,9 @@ def create_table_of_contents(csv_data):
         text_file.write("Table of Contents \n")
         text_file.write("\n")
         for i in csv_data:
-            if i["Issue Type"] != "Epic" and i["Issue key"] in inclusion_list:
+            if i["Issue Type"] != "Epic":
+                # if i["Issue key"] in inclusion_list:
+            # if i["Issue Type"] != "Epic" and i["Issue key"]:
                 table_of_contents[i["Issue key"]] = i["Summary"]
         for key, value in table_of_contents.items():
             text_file.write(key + "\n")
@@ -56,10 +64,12 @@ def create_document(csv_data):
 
         for i in csv_data:
             for k, v in i.items():
-                if k in ("Issue key", "Summary", "Issue Type", "Description", "Outward issue link (Blocks)",
-                         "Outward issue link (Cloners)", "Outward issue link (Relates)",
-                         "Custom field (Story point estimate)", "Parent summary", ):
+                # if k in ("Issue key", "Summary", "Issue Type", "Description", "Outward issue link (Blocks)",
+                #          "Outward issue link (Cloners)", "Outward issue link (Relates)",
+                #          "Custom field (Story point estimate)", "Parent summary", ):
+                if k in ("Issue key", "Summary", "Description"):
                     if v != "Epic":
+                        # if v in inclusion_list:
                         row_data[k] = check_string(v)
             create_text_file(row_data, text_file, document)
     except Exception as e:
@@ -67,15 +77,20 @@ def create_document(csv_data):
 
 
 def create_text_file(row_data, text_object, doc_obj):
+    # print(row_data)
+    # {'Summary': 'Versioning for Admin View', 'Issue key': 'GAPS-1', 'Description': 'N/a'}
+    print(text_object)
     try:
-        for key, value in row_data.items():
-            text_object.write(key + "\n")
-            text_object.write(value + "\n")
-            text_object.write("\n")
-            doc_obj.add_paragraph(key, style='Intense Quote')
-            doc_obj.add_paragraph(value, style='Normal')
+        doc_obj.add_paragraph(row_data["Issue key"] + ": " + row_data["Summary"], style='Normal')
+        doc_obj.add_paragraph(row_data["Description"], style='Normal')
+        # for key, value in row_data.items():
+        #     text_object.write(key + "\n")
+        #     text_object.write(value + "\n")
+        #     text_object.write("\n")
+        #     doc_obj.add_paragraph(value, style='Normal')
+            # doc_obj.add_paragraph(value, style='Normal')
 
-        doc_obj.save('Gaps_Itesm.docx')
+        doc_obj.save('Gaps_Items.docx')
     except Exception as e:
         logging.error(str(e))
 
