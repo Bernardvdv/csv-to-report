d5e588aeec774e49ef16068f9aef14d283e08039
bernard <bvdv9075@gmail.com>
2021-04-23 17:15:37 +0100

    Cleaned up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys, logging, time, re, io
 from datetime import datetime
 
-from docx import Document
 from htmldocx import HtmlToDocx
 
 import class_read_csv
@@ -40,29 +39,26 @@ heading_rule = re.compile('(h[1-6].*)')
 def create_document(csv_data):
 
     try:
-        text_file = open("items.txt", "w")
         html_file = open("demofile.html", "w")
         html_file.write("<!DOCTYPE html>\n")
         html_file.write("<html>\n")
         html_file.write("<body>\n")
         row_data = {}
-        document = Document()
-        document.add_heading('Gaps Items', 0)
 
         for key, val in inclusion_list.items():
             html_file.write(f"<h1> {key} </h1>\n")
-            document.add_paragraph(key, style="Heading 1")
             for a in val:
-
                 for i in csv_data:
                     if i["Issue key"] == a:
                         row_data["Issue key"] = check_string(i["Issue key"])
                         row_data["Summary"] = check_string(i["Summary"])
                         row_data["Description"] = check_string(i["Description"])
 
-                create_text_file(row_data, text_file, document, html_file)
+                create_text_file(row_data, html_file)
         html_file.write("</body>\n")
         html_file.write("</html>\n")
+
+        # Convert HTML file to docx
         new_parser = HtmlToDocx()
         new_parser.parse_html_file("demofile.html", "gaps_items_html_converted")
 
@@ -70,20 +66,19 @@ def create_document(csv_data):
         logging.error(str(e))
 
 
-def create_text_file(row_data, text_obj, doc_obj, html_file):
+def create_text_file(row_data, html_file):
 
     try:
-        # doc_obj.add_paragraph(row_data["Issue key"] + ": " + row_data["Summary"], style="Heading 3")
-        # for line in io.StringIO(row_data["Description"]):
-        #     doc_obj.add_paragraph(replace_bold(replace_heading(line)), style="Body Text")
-        # text_obj.write(f"{row_data['Issue key']} : {row_data['Summary']} \n")
-        # text_obj.write(row_data["Description"] + "\n")
-        # text_obj.write("\n")
-        # doc_obj.save('Gaps_items.docx')
-
         html_file.write(f"<h3> {row_data['Issue key']} : {row_data['Summary']} </h3>\n")
         # for line in io.StringIO(row_data["Description"]):
-        html_file.write(f"{replace_ordered_list(replace_unordered_list(replace_image(replace_bold(replace_heading(row_data['Description'])))))}\n")
+        lines = row_data['Description']
+        lines += replace_ordered_list(row_data['Description'])
+        lines += replace_unordered_list(lines)
+        lines += replace_image(lines)
+        lines += replace_bold(lines)
+        lines += replace_heading(lines)
+
+        html_file.write(f"{lines}\n")
     except Exception as e:
         logging.error(str(e))
 
